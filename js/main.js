4713f19cdade1a72b12766cfbbb63cd1fbbb441e
Mees Gelein <meesgelein@gmail.com>
2019-10-25 15:47:02 +0200

    Designing the UI

--- js/main.js
+++ js/main.js
@@ -2,5 +2,19 @@
  * Entry point of the code
  */
 $(document).ready(() => {
-
+    setMoney(0, 0, 0, 0);
 });
+
+/**
+ * Set the money for each of the coin types, this changes the UI
+ * @param {int} platinum 
+ * @param {int} gold 
+ * @param {int} silver 
+ * @param {int} copper 
+ */
+function setMoney(platinum, gold, silver, copper){
+    $('#currentPlatinum').html(platinum + " Pp");
+    $('#currentGold').html(platinum + " Gp");
+    $('#currentSilver').html(platinum + " Sp");
+    $('#currentCopper').html(platinum + " Cp");
+}
